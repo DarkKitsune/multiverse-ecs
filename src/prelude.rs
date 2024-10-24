4f86eceb8f910592a3114781aa5b069a289d90c9
Dark Kitsu <cbmuench@hotmail.com>
2024-10-23 19:08:59 -0700

    Prefixed exported names from ggutil

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,2 +1,2 @@
 pub use crate::{define_class, node::*, universe::*};
-pub use ggutil::handle_map::{Handle, HandleMapValues, HandleMapValuesMut};
+pub use ggutil::handle_map::{Handle as EntityHandle, HandleMapValues as EntityHandleMapValues, HandleMapValuesMut as EntityHandleMapValuesMut};
